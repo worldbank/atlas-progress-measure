287944a8172c77aa0dcf5cb20664d51a2138e63b
Maarten <maartenzam@gmail.com>
2025-11-03 10:35:25 +0100

    Add number of countries that reached target

--- write_to_sheets.R
+++ write_to_sheets.R
@@ -9,8 +9,8 @@ write_sheet(agg, ss = '14kDeXh0H6vjzldndSBN4p67CCNANHdm7EmqnwojcKh0', sheet = 'a
 progress <- read.csv('output/progress_sheet.csv')
 write_sheet(progress, ss = '14kDeXh0H6vjzldndSBN4p67CCNANHdm7EmqnwojcKh0', sheet = 'progress')
 
-meta <- read.csv('output/meta_sheet.csv')
-write_sheet(meta, ss = '14kDeXh0H6vjzldndSBN4p67CCNANHdm7EmqnwojcKh0', sheet = 'meta')
+#meta <- read.csv('output/meta_sheet.csv')
+#write_sheet(meta, ss = '14kDeXh0H6vjzldndSBN4p67CCNANHdm7EmqnwojcKh0', sheet = 'meta')
 
 meta.extended <- read.csv('output/meta_extended_sheet.csv')
 write_sheet(meta.extended, ss = '14kDeXh0H6vjzldndSBN4p67CCNANHdm7EmqnwojcKh0', sheet = 'meta_extended')
